2a4f8ac3b3c722dff95d8d3318e2252da8b1c8f7
Trung-Kien-2825 <ngkien20082005@gmail.com>
2025-10-04 08:05:13 +0000

    first commit from codespaces

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6c4328b5f58e33a50bfffc0ce3366c3ffdf696ef',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6c4328b5f58e33a50bfffc0ce3366c3ffdf696ef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'aiven/aiven-laravel' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '14a27f069a3dec66420347ba670088dbfd1055e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aiven/aiven-laravel',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
